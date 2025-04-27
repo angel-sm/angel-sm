608f58f1f4efd4543ebfbacfb8591fedd3dcda05
angel-sm <jose.angel.pacheco9729@gmail.com>
2025-04-26 20:58:29 -0500

    Change project presentation

--- src/components/molecules/navbar/Navbar.tsx
+++ src/components/molecules/navbar/Navbar.tsx
@@ -2,13 +2,13 @@ import React, { ReactNode } from "react";
 import Logo from "../../atoms/logo/Logo";
 
 type Props = {
-  menu: ReactNode
-}
+  menu: ReactNode;
+};
 
 const Navbar: React.FC<Props> = ({ menu }) => {
   return (
-    <nav className="navbar w-full h-[8vh] fixed top-0 item z-[999999]">
-      <div className="controller flex justify-between items-center h-full">
+    <nav className="navbar w-full h-[8vh] fixed top-0 item z-[999999]  bg-[#fff]">
+      <div className="max-w-[80%] lg:max-w-[70%] md:max-w-[90%] m-auto flex justify-between items-center h-full">
         <Logo />
         {menu}
       </div>
--- src/sections/Projects.section.tsx
+++ src/sections/Projects.section.tsx
@@ -1,4 +1,4 @@
-import { IconCode, IconCup, IconDeviceLaptop } from "@tabler/icons-react";
+import FunFacts from "../components/molecules/fun-facts/FunFacts";
 import ProjectCard from "../components/molecules/project-card/ProjectCard";
 
 const ExperienceSection = () => {
@@ -8,58 +8,31 @@ const ExperienceSection = () => {
     </div>
   );
 
-  const Info = [
-    {
-      icon: <IconCup />,
-      title: "Cups of coffe",
-      subtitle: "-",
-    },
-    {
-      icon: <IconCode />,
-      title: "Lines of code",
-      subtitle: "-",
-    },
-    {
-      icon: <IconDeviceLaptop />,
-      title: "Projects",
-      subtitle: "-",
-    },
-  ];
-
   const ProjectGrid = (
-    <div className=" grid-cols-8 grid-rows-12 gap-4 hidden md:grid lg:grid">
-      <div className="bg-red-100 col-span-4 row-span-4">
+    <div className="grid-cols-6 grid-rows-10 gap-4 lg:grid md:grid hidden">
+      <div className="bg-red-100 col-span-3 row-span-3">
         <ProjectCard />
       </div>
-      <div className="col-span-4 col-span-4 col-start-5 p-10">
-        <div className="flex w-full h-full items-center justify-between">
-          {Info.map((info, index) => (
-            <div key={index} className="flex flex-col items-center">
-              <div className="text-4xl">{info.icon}</div>
-              <div className="flex flex-col mt-3">
-                <div className="text-sm">{info.title}</div>
-                <div className="text-lg">{info.subtitle}</div>
-              </div>
-            </div>
-          ))}
+      <div className=" col-span-3 col-start-4 row-start-1">
+        <div className=" flex items-center justify-center gap-10 h-full">
+          <FunFacts />
         </div>
       </div>
-      <div className="bg-red-100 col-span-4 col-span-4 row-span-4 col-start-5 row-start-2">
+      <div className="bg-red-100 col-span-3 row-span-3 col-start-4 row-start-2">
         <ProjectCard />
       </div>
-      <div className="bg-red-100 col-span-5 row-span-4 col-start-4 row-start-6">
+      <div className="bg-red-100 col-span-3 row-span-3 col-start-1 row-start-4">
         <ProjectCard />
       </div>
-      <div className="bg-red-100 col-span-3 row-span-5 col-start-1 row-start-5">
+      <div className="bg-red-100 col-span-3 row-span-3 col-start-4 row-start-5">
         <ProjectCard />
       </div>
-      <div className="bg-red-100 col-span-4 row-span-3 col-start-2 row-start-10">
+      <div className="bg-red-100 col-span-3 row-span-3 col-start-1 row-start-7">
         <ProjectCard />
       </div>
-      <div className="bg-red-100 col-span-3 row-span-3 col-start-6 row-start-10">
+      <div className="bg-red-100 col-span-3 row-span-3 col-start-4 row-start-8">
         <ProjectCard />
       </div>
-      <div className="bg-stone-100 row-span-3 col-start-1 row-start-10"></div>
     </div>
   );
 
